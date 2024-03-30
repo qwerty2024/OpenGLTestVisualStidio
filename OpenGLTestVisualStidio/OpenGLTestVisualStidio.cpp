2d30ecb53bc7f3974ecb52db2a701138794a3202
qwerty2024 <multanmgu@gmail.com>
2024-03-30 21:05:20 +0300

    Add GLAD.

--- OpenGLTestVisualStidio/OpenGLTestVisualStidio.cpp
+++ OpenGLTestVisualStidio/OpenGLTestVisualStidio.cpp
@@ -1,6 +1,6 @@
-﻿#include <iostream>
-#include <GLFW\glfw3.h>
-//#include <glad/glad.h>
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
 
 int main(void)
@@ -22,20 +22,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // Init Glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initalize OpenGL context" << std::endl;
+        return -1;
+    }
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         glBegin(GL_TRIANGLES);
-
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
 
         glVertex2d(0.0, 1.0);
         glVertex2d(1.0, -1.0);
         glVertex2d(-1.0, -1.0);
 
         glEnd();
-
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
